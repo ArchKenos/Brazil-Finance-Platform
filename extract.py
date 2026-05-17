ac5324e9c2527742e82d42ad3c1612e3d3cf342d
ArchKenos <227059261+ArchKenos@users.noreply.github.com>
2026-05-17 21:31:24 +0000

    feat: save bronze data inside a data lake

--- extract.py
+++ extract.py
@@ -1,6 +1,8 @@
 import requests
 from pathlib import Path
 from datetime import datetime
+import os
+import json
 
 #URL de Extração
 
@@ -13,13 +15,24 @@ max_time = today_time.replace(year = today_time.year - 10)
 max_time = str(max_time.strftime ("%d/%m/%Y"))
 today_time = str(today_time.strftime ("%d/%m/%Y"))
 
-def extract_by_serie(code, InitialDate = max_time, FinalDate = today_time):
+
+def extract_by_serie(code: int = 11, InitialDate = max_time, FinalDate = today_time):
     url = f"{prefix}{code}/dados?formato=json&dataInicial={InitialDate}&dataFinal={FinalDate}#"
     try:
         response = requests.get(url)
         response.raise_for_status()
-        return response.json
+        return response.json()
     except requests.exceptions.RequestException as e:
         return() 
 
+def save_bronze_data(data):
+    
+    LAKE_DIR.mkdir(parents=True, exist_ok=True)
+    FILE_PATH = LAKE_DIR /"bronze_financedata.json"
+    with open(FILE_PATH, 'w', encoding="UTF-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
 
+
+if __name__ == "__main__":
+    data = extract_by_serie()
+    save_bronze_data(data)
