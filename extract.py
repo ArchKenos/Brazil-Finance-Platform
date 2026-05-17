a846b6233310beb03136d04cf97aa70fe8eb18ea
ArchKenos <227059261+ArchKenos@users.noreply.github.com>
2026-05-17 21:00:41 +0000

    feat: create function to request specific code and date from the API

--- extract.py
+++ extract.py
@@ -13,12 +13,13 @@ max_time = today_time.replace(year = today_time.year - 10)
 max_time = str(max_time.strftime ("%d/%m/%Y"))
 today_time = str(today_time.strftime ("%d/%m/%Y"))
 
-def extract_by_serie(codigo_serie, dataInicial = max_time, dataFinal = today_time):
-    url = f"{prefix}{codigo_serie}/dados?formato=json&dataInicial={dataInicial}&dataFinal={dataFinal}#"
+def extract_by_serie(code, InitialDate = max_time, FinalDate = today_time):
+    url = f"{prefix}{code}/dados?formato=json&dataInicial={InitialDate}&dataFinal={FinalDate}#"
     try:
         response = requests.get(url)
         response.raise_for_status()
         return response.json
     except requests.exceptions.RequestException as e:
         return() 
-    
+
+
