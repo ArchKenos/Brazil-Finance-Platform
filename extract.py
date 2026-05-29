e51232bc4ee76abc7b241332a6069f4ad9844aa6
ArchKenos <227059261+ArchKenos@users.noreply.github.com>
2026-05-29 21:11:43 +0000

    refactor: change code to gather stock_code data from API instead of manually downloaded file

--- extract.py
+++ extract.py
@@ -5,6 +5,7 @@ import pandas as pd
 import os
 import json
 import yfinance as yf
+import base64
 
 #URL de Extração
 
@@ -33,25 +34,35 @@ def save_bronze_data(data):
     with open(FILE_PATH, 'w', encoding="UTF-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
+def return_ibovespa_info_json():
+    base_url = "https://sistemaswebb3-listados.b3.com.br/indexProxy/indexCall/GetPortfolioDay/"
+    parameters_url = {"language":"pt-br",
+                      "pageNumber":"1",
+                      "pageSize":"120",
+                      "index":"IBOV",
+                      "segment":"1"
+                      }
+    parameters_json = json.dumps(parameters_url)
     
+    parameterb64 = base64.urlsafe_b64encode(parameters_json.encode('utf-8')).decode('utf-8')
+
+    api_url = f'{base_url}{parameterb64}'
+    response = requests.get(api_url)
+    response.raise_for_status()
+    response = response.json()
+    
+    return response
+
 def return_ibovespa_stocks():
 
-    #CSV FILE with all ibovespa stock codes
-    df = pd.read_csv(r"/home/archkenos/Downloads/IBOVDia_20-05-26.csv",
-                     encoding='latin1',
-                     header=1,
-                     sep=';',
-                     index_col=False
-                     )
-    column_data = df.iloc[:, 0]
-    ibovespa_stocks = []
+    response = return_ibovespa_info_json()
+    df = pd.DataFrame(response["results"])
+    stocks = df["cod"].dropna().tolist()
     
-    for ticket in column_data:
-        formatted_ticket = f"{ticket}.SA"
-        ibovespa_stocks.append(formatted_ticket)
+    
+    formatted_stocks = [f"{cod}.SA" for cod in stocks] 
         
-    return ibovespa_stocks
-        
+    print(formatted_stocks)
 if __name__ == "__main__":
     return_ibovespa_stocks()
     #data = extract_by_serie()
